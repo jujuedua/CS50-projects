60c035cf32df24415d2938e2c56b41ab92c501f3
Julia Eduarda Mont-Mor Silva <149541870+jujuedua@users.noreply.github.com>
2024-12-19 19:13:20 -0300

    Add files via upload

---
+++ sentimental-readability/readability.py
@@ -0,0 +1,32 @@
+from cs50 import get_string
+
+text = get_string("Text: ")
+
+letters = 0
+space = 0
+sentences = 0
+
+for characters in text:
+    if characters.isalpha():
+        letters += 1
+    elif characters == ' ':
+        space += 1
+    elif characters == '!' or characters == '?' or characters == '.':
+        sentences += 1
+
+words = space + 1
+
+
+L = (letters / words) * 100
+S = (sentences / words) * 100
+
+
+grade = 0.0588 * L - 0.296 * S - 15.8
+
+
+if grade < 1:
+    print("Before Grade 1")
+elif grade > 16:
+    print("Grade 16+")
+else:
+    print(f"Grade {round(grade)}")
