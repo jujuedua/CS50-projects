c0b8616b1e77bec8cedc63e4caae55807e8dc700
Julia Eduarda Mont-Mor Silva <149541870+jujuedua@users.noreply.github.com>
2024-12-19 19:12:23 -0300

    Add files via upload

---
+++ scrabble/scrabble.c
@@ -0,0 +1,49 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int score(string word);
+
+int main(void)
+
+{
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    int score1 = score(word1);
+    int score2 = score(word2);
+
+    if (score1 > score2)
+    {
+        printf("Player 1 Wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 Wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+}
+
+int score(string word)
+{
+    int score = 0;
+
+    for (int i = 0; i < strlen(word); i++)
+    {
+        if (isupper(word[i]))
+        {
+            score = score + POINTS[word[i] - 65];
+        }
+        if (islower(word[i]))
+        {
+            score = score + POINTS[word[i] - 97];
+        }
+    }
+    return score;
+}
